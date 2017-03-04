5e2c52967b30c5f68eddeebe2398b5d381559a4f
Simon Asika <asika32764@gmail.com>
2017-03-04 13:43:27 +0800

    First commit

---
+++ test/test.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once __DIR__ . '/../vendor/autoload.php';
+
+class Foo extends \Asika\SimpleConsole\Console
+{
+    protected $help = <<<HELP
+[Usage] test.php
+
+[Options]
+    h | help    Show help info.
+HELP;
+}
+
+$app = new Foo;
+$app->execute(function ($app)
+{
+    $this->out('Hello');
+});
